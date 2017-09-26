494dd2175ddeff7e14d66b4f8c0c3cf0f26dd335
rteodoro <rteodoro@i2.com.br>
2017-09-26 16:30:20 -0300

    Relatorio com analise e novos fontes separando os mapas por sentimento

--- rirabalhofinal/exibir_maps.py
+++ rirabalhofinal/exibir_maps.py
@@ -10,52 +10,72 @@ GoogleMaps(app, key="")
 
 @app.route("/")
 def mapview():
-    """""
-    Ref: https://stackoverflow.com/questions/44218836/python-flask-googlemaps-get-users-current-location-latitude-and-longitude
-    http://flaskgooglemaps.pythonanywhere.com/
-    https://maps.googleapis.com/maps/api/geocode/json?address=brazil
-    :return:
-    """
-    # locations = [(-9.6498487, -35.7089492), (-22.9068467, -43.1728965)]
-    locations = []
-    with open('dados/resultado2a.csv', 'r', encoding="utf8") as csvfile:
+    locationsrede45 = []
+    locationspt = []
+    locationspmdb = []
+
+    with open('resultado2a.csv', 'r', encoding="utf8") as csvfile:
         reader = csv.DictReader(csvfile, delimiter=';', quotechar='"', lineterminator='\n')
         try:
             for row in reader:
                 if row['formatted_address'] and not row['formatted_address'].startswith('Brazil'):
-                    if row['partido'] == 'Rede45':
-                        icone = 'http://maps.google.com/mapfiles/ms/icons/blue-dot.png'
-                    elif row['partido'] == 'ptbrasil':
-                        icone = 'http://maps.google.com/mapfiles/ms/icons/red-dot.png'
-                    else:
-                        icone = 'http://maps.google.com/mapfiles/ms/icons/green-dot.png'
 
-                    locations.append({'lat': row['lat'], 'lng': row['lng'], 'infobox': row['formatted_address'],
-                                      'icon': icone})
+                    if row['partido'] == 'Rede45':
+                        locationsrede45.append({'lat': row['lat'], 'lng': row['lng'], 'infobox': row['formatted_address'], 'icon': 'http://maps.google.com/mapfiles/ms/icons/blue-dot.png'})
+                    elif row['partido'] == 'ptbrasil':
+                        locationspt.append({'lat': row['lat'], 'lng': row['lng'], 'infobox': row['formatted_address'], 'icon': 'http://maps.google.com/mapfiles/ms/icons/red-dot.png'})
+                    else:
+                        locationspmdb.append({'lat': row['lat'], 'lng': row['lng'], 'infobox': row['formatted_address'], 'icon': 'http://maps.google.com/mapfiles/ms/icons/green-dot.png'})
+
         except Exception as e:
             print('ERRO %s' % str(e))
-    mapa = Map(
-        identifier="view-mapa",
+
+    maparede45 = Map(
+        identifier="view-maparede45",
         varname="mapa",
         style="height:600px;width:1024px;margin:0;",
         lat=-14.235004,
         lng=-51.92528,
         zoom=5,
-        markers=locations
+        markers=locationsrede45
     )
-    return render_template('template.html', mapa=mapa)
+
+    mapapt = Map(
+        identifier="view-mapapt",
+        varname="mapa",
+        style="height:600px;width:1024px;margin:0;",
+        lat=-14.235004,
+        lng=-51.92528,
+        zoom=5,
+        markers=locationspt
+    )
+
+    mapapmdb = Map(
+        identifier="view-mapapmdb",
+        varname="mapa",
+        style="height:600px;width:1024px;margin:0;",
+        lat=-14.235004,
+        lng=-51.92528,
+        zoom=5,
+        markers=locationspmdb
+    )
+
+    return render_template('template.html', maparede45=maparede45, mapapt=mapapt, mapapmdb=mapapmdb)
 
 
 @app.route("/palavras")
 def palavras():
     locationsrede45 = []
+    locationsrede45neg = []
     locationspt = []
+    locationsptneg = []
     locationspmdb = []
+    locationspmdbneg = []
     qtd_pos_rede45 = qtd_neg_rede45 = 0
     qtd_pos_pt = qtd_neg_pt = 0
     qtd_pos_pmdb = qtd_neg_pmdb = 0
 
-    with open('dados/palavras_location_separadas2.csv', 'r', encoding="utf8") as csvfile:
+    with open('palavras_location_separadas2.csv', 'r', encoding="utf8") as csvfile:
         reader = csv.DictReader(csvfile, delimiter=';', quotechar='"', lineterminator='\n')
         try:
             for row in reader:
@@ -66,26 +86,34 @@ def palavras():
                     icone = 'http://maps.google.com/mapfiles/ms/icons/red-dot.png'
 
                 if row['partido'] == 'Rede45':
-                    locationsrede45.append({'lat': row['lat'], 'lng': row['lng'], 'infobox': row['palavra'],
-                                            'icon': icone})
+
                     if row['tipo'] == 'pos':
+                        locationsrede45.append({'lat': row['lat'], 'lng': row['lng'], 'infobox': row['palavra'],
+                                                'icon': icone})
                         qtd_pos_rede45 += 1
                     else:
+                        locationsrede45neg.append({'lat': row['lat'], 'lng': row['lng'], 'infobox': row['palavra'],
+                                                   'icon': icone})
                         qtd_neg_rede45 += 1
 
                 elif row['partido'] == 'ptbrasil':
-                    locationspt.append(
-                        {'lat': row['lat'], 'lng': row['lng'], 'infobox': row['palavra'], 'icon': icone})
+
                     if row['tipo'] == 'pos':
+                        locationspt.append(
+                            {'lat': row['lat'], 'lng': row['lng'], 'infobox': row['palavra'], 'icon': icone})
                         qtd_pos_pt += 1
                     else:
+                        locationsptneg.append(
+                            {'lat': row['lat'], 'lng': row['lng'], 'infobox': row['palavra'], 'icon': icone})
                         qtd_neg_pt += 1
                 else:
-                    locationspmdb.append(
-                        {'lat': row['lat'], 'lng': row['lng'], 'infobox': row['palavra'], 'icon': icone})
                     if row['tipo'] == 'pos':
+                        locationspmdb.append(
+                            {'lat': row['lat'], 'lng': row['lng'], 'infobox': row['palavra'], 'icon': icone})
                         qtd_pos_pmdb += 1
                     else:
+                        locationspmdbneg.append(
+                            {'lat': row['lat'], 'lng': row['lng'], 'infobox': row['palavra'], 'icon': icone})
                         qtd_neg_pmdb += 1
 
         except Exception as e:
@@ -98,7 +126,19 @@ def palavras():
         lat=-14.235004,
         lng=-51.92528,
         zoom=4,
-        markers=locationsrede45
+        markers=locationsrede45,
+        cluster=False
+    )
+
+    maparede45neg = Map(
+        identifier="view-maparede45neg",
+        varname="maparede45",
+        style="height:600px;width:1024px;margin:0;",
+        lat=-14.235004,
+        lng=-51.92528,
+        zoom=4,
+        markers=locationsrede45neg,
+        cluster=False
     )
 
     maparedept = Map(
@@ -109,6 +149,15 @@ def palavras():
         lng=-51.92528,
         zoom=4,
         markers=locationspt
+    )
+    maparedeptneg = Map(
+        identifier="view-mapaptneg",
+        varname="maparede45",
+        style="height:600px;width:1024px;margin:0;",
+        lat=-14.235004,
+        lng=-51.92528,
+        zoom=4,
+        markers=locationsptneg
     )
 
     maparedepmdb = Map(
@@ -121,7 +170,20 @@ def palavras():
         markers=locationspmdb
     )
 
-    return render_template('palavras.html', maparede45=maparede45, maparedept=maparedept, maparedepmdb=maparedepmdb,
+    maparedepmdbneg = Map(
+        identifier="view-mapapmdbng",
+        varname="maparede45",
+        style="height:600px;width:1024px;margin:0;",
+        lat=-14.235004,
+        lng=-51.92528,
+        zoom=4,
+        markers=locationspmdbneg
+    )
+
+    return render_template('palavras.html',
+                           maparede45=maparede45, maparede45neg=maparede45neg,
+                           maparedept=maparedept, maparedeptneg=maparedeptneg,
+                           maparedepmdb=maparedepmdb, maparedepmdbneg=maparedepmdbneg,
                            qtd_pos_rede45=qtd_pos_rede45, qtd_neg_rede45=qtd_neg_rede45,
                            qtd_pos_pt=qtd_pos_pt, qtd_neg_pt=qtd_neg_pt,
                            qtd_pos_pmdb=qtd_pos_pmdb, qtd_neg_pmdb=qtd_neg_pmdb)
